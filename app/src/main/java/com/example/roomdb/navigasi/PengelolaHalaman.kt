2169425c45e8367647832bc0524feca6ca7b8ac3
enjoyghozi <adamghosi03@gmail.com>
2023-12-05 20:24:59 +0700

    Create function HostNavigasi

--- app/src/main/java/com/example/roomdb/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomdb/navigasi/PengelolaHalaman.kt
@@ -12,7 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.roomdb.R
+import com.example.roomdb.ui.theme.halaman.DestinasiEntry
+import com.example.roomdb.ui.theme.halaman.DestinasiHome
+import com.example.roomdb.ui.theme.halaman.EntrySiswaScreen
+import com.example.roomdb.ui.theme.halaman.HomeScreen
 
 
 @Composable
@@ -45,3 +52,25 @@ fun SiswaTopAppBar(
         }
     )
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigasiBack = { navController.popBackStack()})
+        }
+    }
+}
+
